195e804faf4736b60696da41f2d54fd53d35ec2c
brew <76052198@qq.com>
2016-04-26 11:39:18 +0800

    set iface

--- mfront/src/main/java/onight/zjfae/mfront/action/IFEProxyAction.java
+++ mfront/src/main/java/onight/zjfae/mfront/action/IFEProxyAction.java
@@ -63,12 +63,9 @@ public class IFEProxyAction extends MobileModuleStarter<Message> {
 			} else {
 				try {
 					currentBuilder.set(builder);
-					JsonPBUtil.json2PB(pack.getBody(), builder);
-					//builder.build();// 获取到的是一个PBmessage
-					
 					Message msg = getPBBody(pack);//
 					//1.preprocess.== validate..前处理逻辑
-					
+					log.debug("msg=="+msg);
 					try{
 						bmap.preProcess(msg, pbname);	
 					}catch(Exception e){
@@ -78,42 +75,28 @@ public class IFEProxyAction extends MobileModuleStarter<Message> {
 					log.debug("msg=={}",msg);
 					String str = new FJsonPBFormat().printToString(msg);
 					log.debug("proxy:{}", str);
-					if(str.indexOf("returnCode")>=0){
-						Builder retbuilder = (Builder) bmap.getResBuilder(pbname);
-						JsonPBUtil.json2PB(str.getBytes("UTF-8"), retbuilder);
-
-						handler.onFinished(PacketHelper.toPBReturn(pack, retbuilder.build()));
-					}
-
 					String path = bmap.getEURL(pbname);
-				
 					log.debug("name:"+pbname+",url="+path);
-					
-					pack.setBody(str.getBytes("UTF-8"));
 					// 1. 把他封装成json,并且发到客户端E/工程里面去
-					String requestBody = jsons.serialize(pack.getBody()).toString();
+					String requestBody = str;
 					String jsonStr = requestor.post(requestBody,"http://10.18.13.104"+path);
-					
 					//报文格式整理
 					String sub=jsonStr.substring(jsonStr.indexOf(":")+1, jsonStr.lastIndexOf("}"));
-					
 					System.out.println(sub);
-					
 					//  2. json,转换成PBMessage再发到客户端
 					Builder retbuilder = (Builder) bmap.getResBuilder(pbname);
 					JsonPBUtil.json2PB(sub.getBytes("UTF-8"), retbuilder);
-					
 					Message retmsg = retbuilder.build();
-					
 					//postprocess 后处理逻辑
 					try{
 						bmap.postProcess(retmsg, pbname);	
+						handler.onFinished(PacketHelper.toPBReturn(pack, retmsg));
+
 					}catch(Exception e){
 						handler.onFinished(PacketHelper.toPBReturn(pack, new SendFailedBody("消息后置处理："+e.getMessage(), null)));
 					}
 				
 	
-					handler.onFinished(PacketHelper.toPBReturn(pack, retmsg));
 					
 					
 				} catch (Exception e) {
