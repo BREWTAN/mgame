4af2c260b770f50877e2c172c836c8e8c3690422
wangdongguang <wangdongguang@wangdongguangdeMacBook-Pro.local>
2016-06-15 10:27:40 +0800

    git add 新增字段：comparison

--- mfront/src/main/java/onight/mgame/utils/ProtoSwiftGens.java
+++ mfront/src/main/java/onight/mgame/utils/ProtoSwiftGens.java
@@ -133,7 +133,7 @@ public class ProtoSwiftGens {
 				if (ano == null)
 					continue;
 //				System.out.println(clazz);
-				if(!clazz.getSimpleName().equals("IF_持仓信息查询"))continue;
+				if(!clazz.getSimpleName().equals("IF_预约认购产品详情查询"))continue;
 
 				System.out.println("ano==" + ano.name() + ",path=" + ano.path());
 				StringBuffer sb = new StringBuffer();
